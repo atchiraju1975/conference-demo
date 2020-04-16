f5e302f2c9f4355cf75ed301b8534422d09d8314
SrinivasaChakka <Srinivasa.Chakka@tesco.com>
2020-04-16 06:14:14 +0530

    using java class configuration
    overriding application properties file using java configuration

---
+++ src/main/java/com/pluralsight/conferencedemo/config/persistenceConfig.java
@@ -0,0 +1,22 @@
+package com.pluralsight.conferencedemo.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.util.Locale;
+
+@Configuration
+public class persistenceConfig {
+
+    @Bean
+    public DataSource dataSource(){
+        DataSourceBuilder builder = DataSourceBuilder.create();
+        builder.url("jdbc:postgresql://localhost:5432/conference_app");
+        builder.username("postgres");
+        builder.password("Welcome");
+        System.out.println("building application properties using data source class");
+        return builder.build();
+    }
+}
